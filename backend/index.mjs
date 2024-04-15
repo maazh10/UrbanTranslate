b3ecc847964dc2a399a21a7856d28f2e943aaff2
Maaz Hashmi <mhashmi373@gmail.com>
2024-04-15 03:21:02 -0400

    feat: added api to URL; health check endpoint at root

--- backend/index.mjs
+++ backend/index.mjs
@@ -51,7 +51,11 @@ const translate = async (sentence, words, definitions) => {
     return translation.replace(/\n/g, '').trim();
 }
 
-app.get('/define/:word', async (req, res) => {
+app.get('/api/', (req, res) => {
+    res.send('Welcome to Urban Translate API');
+});
+
+app.get('api/define/:word', async (req, res) => {
     const word = req.params.word;
     if (!word)
         res.status(400).send('Word parameter is required');
@@ -59,7 +63,7 @@ app.get('/define/:word', async (req, res) => {
     res.send(def);
 });
 
-app.get('/translate/:sentence', async (req, res) => {
+app.get('api/translate/:sentence', async (req, res) => {
     const sentence = req.params.sentence;
     if (!sentence)
         res.status(400).send('Sentence parameter is required');
